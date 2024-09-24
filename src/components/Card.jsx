e909670975e7434540c388f3a463c488a7ab4682
Abidzarzlfr <abidzarbijeh@gmail.com>
2024-09-24 16:51:01 +0700

    feat(assets, components, pages): add new assets for MyProjects.jsx pages and update passing multiple props at Card.jsx component

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,19 +1,22 @@
 import React from "react";
 
-const Card = () => {
+const Card = (props) => {
+
+  // console.log("ini props card", props);
+
   return (
     <div className="card card-compact bg-base-100 w-96 shadow-xl">
       <figure>
         <img
-          src="https://media.tarkett-image.com/large/TH_25094225_25187225_001.jpg"
-          alt="Shoes"
+          src={props.img}
+          alt="Project-Image"
         />
       </figure>
       <div className="card-body">
-        <h2 className="card-title">Shoes!</h2>
+        <h2 className="card-title">{props.title}</h2>
         <p>If a dog chews shoes whose shoes does he choose?</p>
         <div className="card-actions justify-end">
-          <button className="btn btn-primary">Buy Now</button>
+          <button className="btn btn-accent btn-outline">Buy Now</button>
         </div>
       </div>
     </div>
--- src/pages/MyProjects.jsx
+++ src/pages/MyProjects.jsx
@@ -1,6 +1,12 @@
 import React from "react";
 import Card from "../components/Card";
 import Title from "../components/Title";
+import Fitclub from "../assets/myProjects/fitclub-mockup.png";
+import Kelontong from "../assets/myProjects/kelontong-mockup.png";
+import Sapatuku from "../assets/myProjects/sapatuku-mockup.png";
+import Restaurant from "../assets/myProjects/restaurant-mockup.png";
+import Pelita from "../assets/myProjects/pelita-mockup.jpg";
+import Pentiing from "../assets/myProjects/pentiing-mockup.png";
 
 export default function MyProjects() {
   return (
@@ -9,12 +15,12 @@ export default function MyProjects() {
         <Title value="My Projects"/>
       </div>
       <div className="myprojects-content flex-wrap flex gap-x-8 gap-y-4 flex-cols-3 justify-center">
-        <Card />
-        <Card />
-        <Card />
-        <Card />
-        <Card />
-        <Card />
+        <Card img={Fitclub} title={"Fitclub"}/>
+        <Card img={Pelita} title={"Pelita"}/>
+        <Card img={Restaurant} title={"Restaurant"}/>
+        <Card img={Kelontong} title={"Kelontong"}/>
+        <Card img={Sapatuku} title={"Sapatuku"}/>
+        <Card img={Pentiing} title={"Pentiing"}/>
       </div>
     </div>
   );
