605080f1099d9c69167ef56381b0a8f61d66f238
Abidzarzlfr <abidzarbijeh@gmail.com>
2024-09-24 22:26:11 +0700

    feat(page, component, asset): update new MyProjects.jsx, Card.jsx, and asset

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,22 +1,31 @@
 import React from "react";
 
 const Card = (props) => {
-
   // console.log("ini props card", props);
 
   return (
     <div className="card card-compact bg-base-100 w-96 shadow-xl">
       <figure>
-        <img
-          src={props.img}
-          alt="Project-Image"
-        />
+        <img src={props.img} alt="Project-Image" />
       </figure>
       <div className="card-body">
-        <h2 className="card-title">{props.title}</h2>
-        <p>If a dog chews shoes whose shoes does he choose?</p>
-        <div className="card-actions justify-end">
-          <button className="btn btn-accent btn-outline">Buy Now</button>
+        <h2 className="text-lg font-bold">{props.title}</h2>
+        <p>
+          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Architecto
+          autem facilis placeat consequatur eum, sapiente enim sint ab unde
+          recusandae voluptatem iusto, excepturi praesentium obcaecati
+          similique, aperiam provident modi. Cumque.
+        </p>
+        <div className="card-bottom flex">
+          <div className="card-bottom-start pt-2 px-2 flex gap-3 rounded-md bg-base-300">
+            {props.firstIcon}
+            {props.secondIcon}
+            {props.thirdIcon}
+            {props.fourthIcon}
+          </div>
+          <div className="card-bottom-end ml-auto">
+            <button className="btn btn-accent btn-outline">Buy Now</button>
+          </div>
         </div>
       </div>
     </div>
--- src/pages/MyProjects.jsx
+++ src/pages/MyProjects.jsx
@@ -7,20 +7,81 @@ import Sapatuku from "../assets/myProjects/sapatuku-mockup.png";
 import Restaurant from "../assets/myProjects/restaurant-mockup.png";
 import Pelita from "../assets/myProjects/pelita-mockup.jpg";
 import Pentiing from "../assets/myProjects/pentiing-mockup.png";
+import Portfolio from "../assets/myProjects/portfolio-mockup.png";
+import { FaBootstrap, FaLaravel, FaNodeJs, FaReact } from "react-icons/fa";
+import { BiLogoJavascript } from "react-icons/bi";
+import { SiMysql, SiPhp, SiVite } from "react-icons/si";
+import { RiTailwindCssFill } from "react-icons/ri";
 
 export default function MyProjects() {
+  const reactIcon = <FaReact className="text-3xl text-cyan-400" />;
+  const nodeIcon = <FaNodeJs className="text-3xl text-green-400" />;
+  const jsIcon = <BiLogoJavascript className="text-3xl text-yellow-400" />;
+  const laravelIcon = <FaLaravel className="text-3xl text-red-600" />;
+  const phpIcon = <SiPhp className="text-3xl text-indigo-300" />;
+  const bootstrapIcon = <FaBootstrap className="text-3xl text-violet-600" />;
+  const mysqlIcon = <SiMysql className="text-3xl text-blue-400" />;
+  const viteIcon = <SiVite className="text-3xl text-yellow-300" />;
+  const tailwindIcon = <RiTailwindCssFill className="text-3xl text-cyan-500" />;
+
   return (
     <div className="myprojects-page mt-16">
       <div className="myprojects-title text-center mb-10">
-        <Title value="My Projects"/>
+        <Title value="My Projects" />
       </div>
       <div className="myprojects-content flex-wrap flex gap-x-8 gap-y-4 flex-cols-3 justify-center">
-        <Card img={Fitclub} title={"Fitclub"}/>
-        <Card img={Pelita} title={"Pelita"}/>
-        <Card img={Restaurant} title={"Restaurant"}/>
-        <Card img={Kelontong} title={"Kelontong"}/>
-        <Card img={Sapatuku} title={"Sapatuku"}/>
-        <Card img={Pentiing} title={"Pentiing"}/>
+        <Card
+          img={Fitclub}
+          title={"Gym Exercise Website"}
+          firstIcon={reactIcon}
+          secondIcon={nodeIcon}
+          thirdIcon={jsIcon}
+        />
+        <Card
+          img={Pelita}
+          title={"Toodler Health Website"}
+          firstIcon={laravelIcon}
+          secondIcon={phpIcon}
+          thirdIcon={mysqlIcon}
+          fourthIcon={bootstrapIcon}
+        />
+        <Card
+          img={Portfolio}
+          title={"Portfolio Website"}
+          firstIcon={reactIcon}
+          secondIcon={nodeIcon}
+          thirdIcon={jsIcon}
+          fourthIcon={tailwindIcon}
+        />
+        <Card
+          img={Restaurant}
+          title={"Open Source Restaurant Website"}
+          firstIcon={reactIcon}
+          secondIcon={nodeIcon}
+          thirdIcon={jsIcon}
+        />
+        <Card
+          img={Kelontong}
+          title={"Grocery Store Website"}
+          firstIcon={laravelIcon}
+          secondIcon={phpIcon}
+          thirdIcon={mysqlIcon}
+          fourthIcon={bootstrapIcon}
+        />
+        <Card
+          img={Sapatuku}
+          title={"Shoe Shop Website"}
+          firstIcon={viteIcon}
+          secondIcon={bootstrapIcon}
+        />
+        <Card
+          img={Pentiing}
+          title={"Parenting Website"}
+          firstIcon={laravelIcon}
+          secondIcon={phpIcon}
+          thirdIcon={mysqlIcon}
+          fourthIcon={bootstrapIcon}
+        />
       </div>
     </div>
   );
