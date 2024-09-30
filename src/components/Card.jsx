455320b6eacadfc22a550d12b7da5ced29416f14
Abidzarzlfr <abidzarbijeh@gmail.com>
2024-09-30 14:08:09 +0700

    feat(component, page): update MyProjects pages, Card component, and implement clean code structure

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,7 +1,14 @@
 import React from "react";
+import { LuExternalLink } from "react-icons/lu";
 
 const Card = (props) => {
-  // console.log("ini props card", props);
+  const handleButtonClick = () => {
+    if (props.url) {
+      window.open(props.url, "_blank");
+    } else {
+      window.location.href = window.location.origin;
+    }
+  };
 
   return (
     <div className="card card-compact bg-base-100 w-96 shadow-xl">
@@ -10,12 +17,7 @@ const Card = (props) => {
       </figure>
       <div className="card-body">
         <h2 className="text-lg font-bold">{props.title}</h2>
-        <p>
-          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Architecto
-          autem facilis placeat consequatur eum, sapiente enim sint ab unde
-          recusandae voluptatem iusto, excepturi praesentium obcaecati
-          similique, aperiam provident modi. Cumque.
-        </p>
+        <p>{props.body}</p>
         <div className="card-bottom flex">
           <div className="card-bottom-start pt-2 px-2 flex gap-3 rounded-md bg-base-300">
             {props.firstIcon}
@@ -24,7 +26,12 @@ const Card = (props) => {
             {props.fourthIcon}
           </div>
           <div className="card-bottom-end ml-auto">
-            <button className="btn btn-accent btn-outline">Buy Now</button>
+            <button
+              className="btn btn-accent btn-outline"
+              onClick={handleButtonClick}
+            >
+              <LuExternalLink className="text-xl" />
+            </button>
           </div>
         </div>
       </div>
--- src/pages/MyProjects.jsx
+++ src/pages/MyProjects.jsx
@@ -1,28 +1,79 @@
 import React from "react";
 import Card from "../components/Card";
 import Title from "../components/Title";
+
+// Project Images
 import Fitclub from "../assets/myProjects/fitclub-mockup.png";
 import Kelontong from "../assets/myProjects/kelontong-mockup.png";
 import Sapatuku from "../assets/myProjects/sapatuku-mockup.png";
 import Restaurant from "../assets/myProjects/restaurant-mockup.png";
 import Pelita from "../assets/myProjects/pelita-mockup.jpg";
-import Pentiing from "../assets/myProjects/pentiing-mockup.png";
 import Portfolio from "../assets/myProjects/portfolio-mockup.png";
+
+// Icons
 import { FaBootstrap, FaLaravel, FaNodeJs, FaReact } from "react-icons/fa";
 import { BiLogoJavascript } from "react-icons/bi";
 import { SiMysql, SiPhp, SiVite } from "react-icons/si";
 import { RiTailwindCssFill } from "react-icons/ri";
 
 export default function MyProjects() {
-  const reactIcon = <FaReact className="text-3xl text-cyan-400" />;
-  const nodeIcon = <FaNodeJs className="text-3xl text-green-400" />;
-  const jsIcon = <BiLogoJavascript className="text-3xl text-yellow-400" />;
-  const laravelIcon = <FaLaravel className="text-3xl text-red-600" />;
-  const phpIcon = <SiPhp className="text-3xl text-indigo-300" />;
-  const bootstrapIcon = <FaBootstrap className="text-3xl text-violet-600" />;
-  const mysqlIcon = <SiMysql className="text-3xl text-blue-400" />;
-  const viteIcon = <SiVite className="text-3xl text-yellow-300" />;
-  const tailwindIcon = <RiTailwindCssFill className="text-3xl text-cyan-500" />;
+  // Technology Icons
+  const icons = {
+    react: <FaReact className="text-3xl text-cyan-400" />,
+    node: <FaNodeJs className="text-3xl text-green-400" />,
+    js: <BiLogoJavascript className="text-3xl text-yellow-400" />,
+    laravel: <FaLaravel className="text-3xl text-red-600" />,
+    php: <SiPhp className="text-3xl text-indigo-300" />,
+    bootstrap: <FaBootstrap className="text-3xl text-violet-600" />,
+    mysql: <SiMysql className="text-3xl text-blue-400" />,
+    vite: <SiVite className="text-3xl text-yellow-300" />,
+    tailwind: <RiTailwindCssFill className="text-3xl text-cyan-500" />,
+  };
+
+  const projects = [
+    {
+      img: Fitclub,
+      title: "Gym Exercise Website",
+      body: "Fitclub is a website that provides workout movement references using an API as its main data source. The purpose of creating this website is to assist the community in selecting appropriate movements when exercising at the gym or at home.",
+      icons: [icons.react, icons.node, icons.js],
+      url: "https://fitclub-id-abidzarzlfr.netlify.app/"
+    },
+    {
+      img: Pelita,
+      title: "Toddler Health Website",
+      body: "A recent project I worked on was my final thesis during university, where I developed the 'PELITA' website. This website was designed to help parents care for their children, particularly toddlers, to reduce the rates of stunting, wasting, and underweight in Indonesia.",
+      icons: [icons.laravel, icons.php, icons.mysql, icons.bootstrap],
+      url: "https://github.com/Abidzarzlfr/PELITA-V2"
+    },
+    {
+      img: Portfolio,
+      title: "Portfolio Website",
+      body: "This website was developed as my digital portfolio, containing information about myself, my CV, academic transcript, social media, technologies I am proficient in, and several highlights of projects I have worked on in software engineering, particularly in web development.",
+      icons: [icons.react, icons.node, icons.js, icons.tailwind],
+      url: ""
+    },
+    {
+      img: Restaurant,
+      title: "Open Source Restaurant Website",
+      body: "This restaurant website promotes a restaurant called Gericht. My work involved implementing the existing open-source design using React.js.",
+      icons: [icons.react, icons.node, icons.js],
+      url: "https://restaurant-abidzar.netlify.app/"
+    },
+    {
+      img: Kelontong,
+      title: "Grocery Store Website",
+      body: "Kelontong is a digital platform that provides information about traditional small grocery stores in Indonesia. It supports micro and macro economies by enhancing SME competitiveness in the retail sector.",
+      icons: [icons.laravel, icons.php, icons.mysql, icons.bootstrap],
+      url: "https://github.com/SI-RPL-2023/SI4404_KEL243_KELONTONG"
+    },
+    {
+      img: Sapatuku,
+      title: "Shoe Shop Website",
+      body: "Sapatuku was a task for a web application development course at Telkom University. It is a buy-and-sell website for boot enthusiasts.",
+      icons: [icons.vite, icons.bootstrap],
+      url: "https://sapatuku.netlify.app/"
+    },
+  ];
 
   return (
     <div className="myprojects-page mt-16">
@@ -30,58 +81,19 @@ export default function MyProjects() {
         <Title value="My Projects" />
       </div>
       <div className="myprojects-content flex-wrap flex gap-x-8 gap-y-4 flex-cols-3 justify-center">
-        <Card
-          img={Fitclub}
-          title={"Gym Exercise Website"}
-          firstIcon={reactIcon}
-          secondIcon={nodeIcon}
-          thirdIcon={jsIcon}
-        />
-        <Card
-          img={Pelita}
-          title={"Toodler Health Website"}
-          firstIcon={laravelIcon}
-          secondIcon={phpIcon}
-          thirdIcon={mysqlIcon}
-          fourthIcon={bootstrapIcon}
-        />
-        <Card
-          img={Portfolio}
-          title={"Portfolio Website"}
-          firstIcon={reactIcon}
-          secondIcon={nodeIcon}
-          thirdIcon={jsIcon}
-          fourthIcon={tailwindIcon}
-        />
-        <Card
-          img={Restaurant}
-          title={"Open Source Restaurant Website"}
-          firstIcon={reactIcon}
-          secondIcon={nodeIcon}
-          thirdIcon={jsIcon}
-        />
-        <Card
-          img={Kelontong}
-          title={"Grocery Store Website"}
-          firstIcon={laravelIcon}
-          secondIcon={phpIcon}
-          thirdIcon={mysqlIcon}
-          fourthIcon={bootstrapIcon}
-        />
-        <Card
-          img={Sapatuku}
-          title={"Shoe Shop Website"}
-          firstIcon={viteIcon}
-          secondIcon={bootstrapIcon}
-        />
-        <Card
-          img={Pentiing}
-          title={"Parenting Website"}
-          firstIcon={laravelIcon}
-          secondIcon={phpIcon}
-          thirdIcon={mysqlIcon}
-          fourthIcon={bootstrapIcon}
-        />
+        {projects.map((project, index) => (
+          <Card
+            key={index}
+            img={project.img}
+            title={project.title}
+            body={project.body}
+            firstIcon={project.icons[0]}
+            secondIcon={project.icons[1]}
+            thirdIcon={project.icons[2]}
+            fourthIcon={project.icons[3]}
+            url={project.url}
+          />
+        ))}
       </div>
     </div>
   );
