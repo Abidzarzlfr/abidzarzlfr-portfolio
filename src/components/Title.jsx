636958883d8305541676348399a6b771b7ddb778
Abidzarzlfr <abidzarbijeh@gmail.com>
2024-10-01 21:20:09 +0700

    fix(component): fixing title size

--- src/components/Title.jsx
+++ src/components/Title.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const Title = (props) => {
   return (
     <div className="title">
-      <button className="btn btn-wide btn-outline btn-secondary">
+      <button className="btn btn-wide btn-sm btn-outline btn-secondary">
         <code className="text-lg font-extrabold">
           {props.value}
         </code>
